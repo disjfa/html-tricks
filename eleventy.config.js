e6078575e45edaed6d68a339d71389fa7d2da724
disjfa <disjfa@disjfa.nl>
2025-07-14 21:08:40 +0200

    Added tailwind menu

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,13 @@
-import { HtmlBasePlugin } from '@11ty/eleventy'
+import { HtmlBasePlugin } from "@11ty/eleventy";
 
 export default async function (eleventyConfig) {
-    // Configure Eleventy
-    eleventyConfig.addPlugin(HtmlBasePlugin)
+  // Configure Eleventy
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 
-    eleventyConfig.setInputDirectory('_site')
-    eleventyConfig.setOutputDirectory('_dist')
-    eleventyConfig.addWatchTarget('_site/**/*.css')
+  eleventyConfig.setInputDirectory("_site");
+  eleventyConfig.setOutputDirectory("_dist");
+  eleventyConfig.addWatchTarget("_site/**/*.css");
 }
 export const config = {
-    pathPrefix: '/html-tricks/',
-}
+  pathPrefix: "/html-tricks/",
+};
